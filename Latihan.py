f40ac5d57aacd2e0fb935b35e42e85e6e5186902
MaheyzaSaputra <maheyzasaputra20>
2025-02-20 14:59:57 +0700

    menambahkan file pertama dan commit pertama

---
+++ Latihan.py
@@ -0,0 +1,9 @@
+#latihan print text string
+NamaPanggilan = "Ben"
+Umur = "19"
+Alamat = "sukabumi"
+Hobi = "valo"
+print ('Nama: ',NamaPanggilan)
+print ('Umur: ',Umur)
+print ('Alamat: ',Alamat)
+print ('Hobi: ',Hobi)
